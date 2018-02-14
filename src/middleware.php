1d47a8a67cbe5016d722e30b49ec5af0ee743111
Alberto Sosa <inf.albsosa@gmail.com>
2018-02-14 17:52:20 -0600

    uso de los Middleware y los token

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,23 @@
 <?php
-// Application middleware
 
-// e.g: $app->add(new \Slim\Csrf\Guard);
+
+/*  Este middleware 
+lo unico que hace es checar que exista esa cabecera y en caso contrario mostrara error 401, a parte de esto en el archivo route se tiene que poner la siguiente linea en la funcion donde se va a necesitar este middleware 
+
+})->add(new AutMiddleware());
+*/
+class AutMiddleware
+{
+
+    public function __invoke($request, $response, $next)
+    {
+        $token = $request->getHeader('APP-TOKEN');
+        if(empty($token)){
+        	return $response->withStatus(401);
+        }
+        return $response = $next($request, $response);
+       
+
+        
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -11,14 +11,35 @@ $app->get('/[{name}]', function ($request, $response, $args) {
 
 $app->group('/users/', function(){
 	$this->get('getAll', function ($request, $response, $args) {
-		echo '<pre>';
-		print_r($request->getHeaders());
+	/*	echo '<pre>';
+		print_r($request->getHeaders('APP-TOKEN'));
+		echo '</pre>';
+	*/
+	/*
+		//Para alterar el response y mande error 500 los estados http
+		return $response->withStatus(500);
+
+	*/
+
+			$usuarios = [['Nombre' => 'Alberto', 
+						'Apellido'=> 'Sosa'],
+						['Nombre' => 'Beto', 
+						'Apellido'=> 'De la cruz'],
+					];
+					return $response
+					//	->withHeader('Content-type', 'application/json')
+						->write(json_encode($usuarios));
+
 	});
 	$this->get('get/{id}', function ($request, $response, $args) {
 		return "Obteniendo usuario con ID ".$args['id'];
 	});
 	$this->post('insert', function ($request, $response, $args) {
-		return "Se creo un nuevo usuario";
+		print_r($request->getParsedBody());
+
+		$data = $request->getParsedBody();
+
+		return "Se creo un nuevo usuario ".$data['Nombre'];
 	});
 	$this->put('update', function ($request, $response, $args) {
 		return "Se actualizó un usuario";
@@ -27,7 +48,7 @@ $app->group('/users/', function(){
 		return "Se eliminó un usuario con el ID ".$args['id'];
 	});
 
-});
+})->add(new AutMiddleware());
 
 
 
